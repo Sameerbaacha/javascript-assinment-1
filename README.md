41d9c078a004bb1b6f34110fc6c37e34bb10bd13
Sameerbaacha <sameeris6512@gmail.com>
2024-02-26 01:21:17 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javascript-assinment-1
+ 
---
+++ app.js
@@ -0,0 +1,104 @@
+// Assignment 01
+// question 06
+// alert("Sameer  web-developer java-script,c++,python");
+
+
+
+// <!-- ..................       chapter 02     ......................... -->
+
+// Assignment 02
+// Question 01
+//  userName;
+
+
+// Assignment 02
+// Question 02
+// var userName = "Sameer Khan";
+// alert(userName);
+// document.write(userName);
+// console.log(userName);
+
+// Assignment 02
+// Question 03
+// (a)
+// var message;
+// (b)
+// message = "Hello world";
+// (c)
+// alert(message);
+
+
+// Assignment 02
+// Question 04
+// var studentName = "Sameer KHan";
+// var studentAge = "21 years old";
+// var studentfield = "professional web and app developer,IT consultant,enterpreuner and computer scientist";
+
+// alert(studentName);
+// alert(studentAge);
+// alert(studentfield);
+
+// Assignment 02
+// Question 05
+// var name = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(name);
+
+
+
+
+// Assignment 02
+// Question 06
+// var email = "My email address is " + "example@example.com";
+
+// alert(email);
+
+
+// Assignment 02
+// Question 07
+// var book = "I am trying to learn from the book A smarter\n way to learn javascript";
+// alert(book);
+
+
+// Assignment 02
+// Question 08
+// document.write("I can learn javascript i can do it IN SHA ALLAH");
+
+// Assignment 02
+// Question 09
+// var text = "💖´ *•.¸♥¸.•** Sir Ghous Ahmed **•.¸♥¸.•*´💖";
+// document.write(text);
+// alert(text);
+
+// <!-- ............................       chapter 03     ..................................... -->
+
+// Assignment 03
+// Question 01
+// var age = "21 years old";
+// alert(age);
+
+
+// Assignment 03
+// Question 02
+// var visted = "You have visited thus sites 14 times";
+// alert(visted);
+
+
+// Assignment 03
+// Question 03
+// var birthYear="my birth year is 2002 <br>How can I help you today?";
+// document.write(birthYear);
+
+
+
+// Assignment 03
+// Question 04 
+//  (a)
+// var storeDetail = "<b>john doe</b> " + "order <b>5T-shirts(s)</b> " + "XYZ store Karachi";
+// document.write(storeDetail);
+
+
+// // (b)
+// var storeDetail = "<b>john doe</b> ";
+// storeDetail += "order <b>5T-shirts(s)</b> ";
+// storeDetail += "XYZ store Karachi";
+// document.write(storeDetail);
